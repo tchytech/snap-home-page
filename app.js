dfd6aca82a07fe38d84d84bfbeb5cf37c63d6783
Onah Patience Tochukwu <65729360+tchytech@users.noreply.github.com>
2023-08-03 15:46:13 +0100

    done

---
+++ app.js
@@ -0,0 +1,4 @@
+const dropDownButton = document.querySelector('.dropdown-button');
+const arrowIcon = document.querySelector('arrow-icon');
+
+const dropDownContent = document.querySelector('.dropdown-content');
